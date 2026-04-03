ed5ba1a45f7827466854d857ef6239feffc46f06
Abr <habramruizpestana@gmail.com>
2026-04-03 09:10:07 -0400

    Imrpoving NavBar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,12 @@
 // NavBar.tsx
 
 const NavBar = () => (
-    <section className="position-absolute top-0 start-50 translate-middle-x text-light">
-        <div className="container rounded-5"></div>
+    <section className="position-fixed top-0 start-50 translate-middle-x text-light">
+        <div className="container shadow-sm rounded-pill bg-dark border border-1 border-opacity-25 d-flex align-items-center justify-content-center gap-4 py-1 px-5 mt-1">
+            <span>Home</span>
+            <span>Projects</span>
+            <span>Contact</span>
+        </div>
     </section>
 );
 
