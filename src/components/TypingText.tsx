7ca9812ed83c705bd32988e1b27d16dcd388e320
Abr <habramruizpestana@gmail.com>
2026-04-03 21:38:21 -0400

    Imrpoving TypingText component

--- src/components/TypingText.tsx
+++ src/components/TypingText.tsx
@@ -3,27 +3,37 @@ import { useEffect, useState } from "react";
 type Props = {
   text: string;
   speed?: number;
+  highlight: string;
 };
 
-const TypingText = ({ text, speed = 50 }: Props) => {
+const TypingText = ({ text, speed = 50, highlight }: Props) => {
+  const wholeText : string = text + highlight;
   const [displayedText, setDisplayedText] = useState("");
   const [index, setIndex] = useState(0);
+  const showingHighlight = displayedText.length > text.length;
+  const normalPart = displayedText.slice(0, text.length);
+  const highlightPart = displayedText.slice(text.length);
 
   useEffect(() => {
-    if (index < text.length) {
+    if (index < wholeText.length) {
       const timeout = setTimeout(() => {
-        setDisplayedText((prev) => prev + text[index]);
+        setDisplayedText((prev) => prev + wholeText[index]);
         setIndex(index + 1);
       }, speed);
 
       return () => clearTimeout(timeout);
     }
-  }, [index, text, speed]);
+  }, [index, wholeText, speed]);
 
   return (
     <span className="typing">
-      {displayedText}
-      <span className="cursor">|</span>
+    {normalPart}
+    {showingHighlight && (
+      <span className="text-primary">
+      {highlightPart}
+    </span>
+    )}
+    <span className="cursor">|</span> 
     </span>
   );
 };
