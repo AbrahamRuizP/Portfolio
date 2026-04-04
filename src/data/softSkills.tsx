52e8af3a09f1bb9fceff32146a7319edfbdff163
Abr <habramruizpestana@gmail.com>
2026-04-03 21:39:23 -0400

    Creating softSkills array

---
+++ src/data/softSkills.tsx
@@ -0,0 +1,9 @@
+// softSkills.ts
+import { FaBrain, FaComments, FaSearch, FaChess } from "react-icons/fa";
+
+export const softSkills = [
+    { text: "Analytical thinking and problem-solving", icon: <FaBrain /> },
+    { text: "Clear and structured communication", icon: <FaComments /> },
+    { text: "Attention to detail", icon: <FaSearch /> },
+    { text: "Strategic thinking", icon: <FaChess /> },
+  ];
