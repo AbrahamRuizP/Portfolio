8b3b77ef7cb23c9dc68d94fb8455e2d35dff2dba
Abr <habramruizpestana@gmail.com>
2026-04-12 18:00:30 -0400

    Completing experience component

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -11,7 +11,7 @@ export const Experience = () => (
             <div className="d-flex flex-column align-items-center">
 
                 {/* COLUMN */}
-                <div className="col-md-8 offset-md-2 col-lg-6 max-auto mb-5">
+                <div className="col-md-10 offset-md-0 col-lg-6 max-auto mb-5">
 
                     <div className="experience-card py-4 w-100">
 
@@ -22,11 +22,16 @@ export const Experience = () => (
                             <div className="text-start fw-semibold small">
                                 Abax
                             </div>
-                            <div className="text-start small">
-                                Java & FileMaker Developer — 3 years
+                            <div className="text-start fs-6">
+                                Java & FileMaker Developer/ Jan 2023 — Jan 2026
                             </div>
                             <div>
-
+                                <ul className="experience-list text-start fs-6 pt-2">
+                                    <li>Developed business management applications using Java, Hilla, and FileMaker.</li>
+                                    <li>Built and maintained solutions used by companies such as Ryptide Music and Alexander Valve.</li>
+                                    <li>Improved operational efficiency by simplifying resource management workflows.</li>
+                                    <li>Increased data reliability and consistency through robust system design and validation.</li>
+                                </ul>
                             </div>
 
                         </motion.div>
