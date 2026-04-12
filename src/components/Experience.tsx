234da99d129a940a5857350b86bab5ef6234185e
Abr <habramruizpestana@gmail.com>
2026-04-12 17:18:48 -0400

    Adding initial style to Experience card

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,9 +1,11 @@
 // Experience.tsx
 import { motion } from "framer-motion";
+import { TitleTag } from "./TitleTag";
 
 export const Experience = () => (
     <section className="py-5 bg-black bg-dots text-light">
         <div className="container">
+            <TitleTag title="Experience" />
 
             {/* ROW */}
             <div className="d-flex flex-column align-items-center">
@@ -11,14 +13,20 @@ export const Experience = () => (
                 {/* COLUMN */}
                 <div className="col-md-8 offset-md-2 col-lg-6 max-auto mb-5">
 
-                    <div className="experience-card py-4">
+                    <div className="experience-card py-4 w-100">
 
                         {/* WRAPPER */}
                         <motion.div className="w-100 h-100">
 
                             {/* COMPANY NAME */}
                             <div className="text-start fw-semibold small">
-                                — "Abax"
+                                Abax
+                            </div>
+                            <div className="text-start small">
+                                Java & FileMaker Developer — 3 years
+                            </div>
+                            <div>
+
                             </div>
 
                         </motion.div>
